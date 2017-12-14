d4a45a69442e765cfff5254e901a0e60a4d74a2a
HashtagF <jjubjub85@gmail.com>
2017-12-14 15:33:01 +0700

    upload

---
+++ firebase.config.js
@@ -0,0 +1,9 @@
+export default {
+    apiKey: 'AIzaSyCyGza4soAA4z44bdEl5HcI3UXsR54eRz8',
+    authDomain: 'u-ba-d180c.firebaseapp.com',
+    databaseURL: 'https://u-ba-d180c.firebaseio.com',
+    projectId: 'u-ba-d180c',
+    storageBucket: 'u-ba-d180c.appspot.com',
+    messagingSenderId: '1094667467042'
+  }
+  
